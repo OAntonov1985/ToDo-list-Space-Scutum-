ce4051742f40bd332cc2bdb414c90013d017eea9
OAntonov1985 <aantonov1985@gmail.com>
2025-01-17 18:43:52 +0200

    17.01/14

---
+++ src/app/form/page.js
@@ -0,0 +1,63 @@
+"use client"
+import { useEffect, useState } from 'react';
+
+export default function Form() {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value
+        }));
+    };
+
+    async function sendData(event) {
+        event.preventDefault()
+        console.log(formData)
+        const url = "https://express-app-rouge-mu.vercel.app/api/users/login";
+        try {
+            const response = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+                credentials: "include",
+            });
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+
+            const json = await response.json();
+            console.log(json);
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
+    return (
+        <form style={{ display: "flex", flexDirection: "column", maxWidth: 300, gap: 20 }}>
+            <label htmlFor="email">Email</label>
+            <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+            />
+            <label htmlFor="password">Password</label>
+            <input
+                type="password"
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+            />
+            <button type='submit' onClick={(event) => sendData(event)}>submit</button>
+        </form>
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,18 @@ import Header from './components/Header/Heaer';
 import Main from './components/Main/Main';
 import { HomeStyle } from './page.style';
 import './globals.css';
+import Head from 'next/head'
 
 export const metadata = {
     title: "ToDo List",
     description: "ToDo List Test Task",
-    icons: './public/icons/plus.svg',
+    icons: './public/icons/444.jpg',
+    icons: {
+        icon: [{
+            url: "./public/icons/444.jpg",
+            href: "./public/icons/444.jpg",
+        }]
+    }
 };
 
 export default function Home() {
